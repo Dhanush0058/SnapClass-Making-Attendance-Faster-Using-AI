f2c53fea53fcc48fa7c8091e1e8278db1ac5f5ff
Dhanush0058 <dhanushr672@gmail.com>
2026-05-30 00:13:13 +0530

    removed color from h2

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -55,8 +55,7 @@ def style_base_layout():
                 font-size: 2rem !important;
                 line-height: 0.9 !important;
                 margin-bottom: 0rem !important;
-                color: black;
-            }
+                }
 
             h3 , h4 , p{
                 font-family:"Outfit" , sans-serif !important;
